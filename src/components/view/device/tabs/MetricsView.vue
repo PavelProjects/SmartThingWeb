d371803416d920ab4d1e4c16a9dc45716de5fd38
PavelProjects <paveloalexeev52@gmail.com>
2023-11-16 00:33:49 +0300

    fixes

--- src/components/view/device/tabs/MetricsView.vue
+++ src/components/view/device/tabs/MetricsView.vue
@@ -2,6 +2,7 @@
 import { DeviceApi } from '../../../../api/device/DeviceApi'
 import InputWithLabel from '../../../fields/InputWithLabel.vue'
 import TreeList from '../../../fields/TreeList.vue'
+import SyncLoader from 'vue-spinner/src/SyncLoader.vue'
 
 export default {
     name: "MetricsView",
@@ -11,10 +12,12 @@ export default {
     },
     components: {
         InputWithLabel,
-        TreeList
+        TreeList,
+        SyncLoader
     },
     data() {
         return {
+            loading: false,
             metrics: {}
         }
     },
@@ -23,7 +26,12 @@ export default {
     },
     methods: {
         async update() {
-            this.metrics = await DeviceApi.metrics(this.ip, this.gateway)
+            this.loading = true
+            try {
+                this.metrics = await DeviceApi.metrics(this.ip, this.gateway)
+            } finally {
+                this.loading = false
+            }
         },
     }
 }
@@ -31,9 +39,10 @@ export default {
 
 <template>
     <h1 class="title">Device metrics</h1>
-   <div class="list">
+    <sync-loader class="loading-spinner" :loading="loading"></sync-loader>
+    <div class="list">
         <TreeList
             :values="metrics"
         />
-   </div> 
+    </div> 
 </template>
