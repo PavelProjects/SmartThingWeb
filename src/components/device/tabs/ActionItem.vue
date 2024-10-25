ce22d17ba35b56201368ee4d226acb38e0ca83d0
PavelProjects <paveloalexeev52@gmail.com>
2024-10-25 16:49:22 +0300

    fixed action title

--- src/components/device/tabs/ActionItem.vue
+++ src/components/device/tabs/ActionItem.vue
@@ -99,7 +99,7 @@ export default {
   <BaseContainer :vertical="false">
     <LoadingButton
       class="action-button"
-      :title="intl.formatMessage({ id: 'device.actions.title' }, action)"
+      :title="intl.formatMessage({ id: 'device.actions.title' }, { callDelay: 0, lastCall: 0, ...action})"
       :loading="loading"
       @click="callAction(action.name)"
     >
