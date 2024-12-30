ef8e84bac932a2994515e76c71880d5db635c30c
PavelProjects <pavelprojects52@gmail.com>
2024-12-30 22:05:31 +0300

    fixed loading buton pulse end

--- src/components/base/controls/LoadingButton.vue
+++ src/components/base/controls/LoadingButton.vue
@@ -7,18 +7,31 @@ export default {
     disabled: Boolean
   },
   emits: ['click'],
+  data() {
+    return {
+      pulse: false
+    }
+  },
+  watch: {
+    loading(value) {
+      if (value) {
+        this.pulse = true
+      }
+    }
+  },
   computed: {
     style() {
+      const style = {}
       if (this.disabled) {
-        return { cursor: 'not-allowed' }
+        style['cursor'] = 'not-allowed'
       }
       if (this.loading) {
-        return {
-          cursor: 'wait',
-          animation: 'shadow-pulse 0.5s infinite alternate'
-        }
+        style['cursor'] = 'wait'
       }
-      return {}
+      if (this.pulse) {
+        style['animation'] = 'shadow-pulse 0.5s infinite alternate'
+      }
+      return style
     }
   }
 }
@@ -30,6 +43,11 @@ export default {
     :id="testId"
     :disabled="loading || disabled"
     :style="style"
+    @animationiteration="
+      () => {
+        if (!loading) pulse = false
+      }
+    "
     @click.stop="() => $emit('click')"
   >
     <slot></slot>
